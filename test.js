57e55a9c45963a46fffffc259bda17472a4933ab
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2024-06-27 14:52:17 +0400

    fix: Fix for generateImageTags functionality

--- test.js
+++ test.js
@@ -21,19 +21,18 @@ async function main() {
   // });
 
   // GET IMAGE DESCRIPTION
-  const [error, response] = await batchGpt.generateImageDescription({
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg/1920px-Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg",
-    level: "low",
-  });
+  // const [error, response] = await batchGpt.generateImageDescription({
+  //   image:
+  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg/1920px-Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg",
+  //   level: "low",
+  // });
 
   // GET IMAGE TAGS
-  // const [error, response] = await batchGpt.generateImageTags({
-  //   image:
-  //     "https://plus.unsplash.com/premium_photo-1661765778256-169bf5e561a6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //   searchTermCount: 5,
-  //   // randomness: 10,
-  // });
+  const [error, response] = await batchGpt.generateImageTags({
+    context: "valley",
+    searchTermCount: 5,
+    // randomness: 10,
+  });
 
   if (!error) {
     console.log("Images generated successfully");
